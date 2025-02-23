a6c1e11496bb8adb22a73f8dee55dedbe4682d86
yan <yan_mvd@hotmail.com>
2025-02-23 15:08:03 -0300

    refactor: :recycle: lint

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
-import z from "zod";
+import z from 'zod'
 
 const envSchema = z.object({
-    PORT: z.coerce.number().default(3333),
-    CORS_ALLOWED_DOMAINS: z.string().default("*")
+  PORT: z.coerce.number().default(3333),
+  CORS_ALLOWED_DOMAINS: z.string().default('*'),
 })
 
 export const env = envSchema.parse(process.env)
--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,26 +1,31 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import z from "zod";
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
-export const subscribeToEventRoute:FastifyPluginAsyncZod = async  (app)  =>{
-    app.post('/subscriptions',{schema:{
-        summary: "subscribes user to the event",
-        tags:["susbscription"],
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'subscribes user to the event',
+        tags: ['susbscription'],
         body: z.object({
-            name: z.string(),
-            email: z.string().email()
+          name: z.string(),
+          email: z.string().email(),
         }),
-        response:{
-            201: z.object({
+        response: {
+          201: z.object({
             name: z.string(),
-            email: z.string()
-        })
-     }
-    }},async (req, reply)=>{
-        const {email,name} = req.body;
-        return reply.status(201).send({
-            name,
-            email
-        });
-    });
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (req, reply) => {
+      const { email, name } = req.body
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    }
+  )
 }
-
