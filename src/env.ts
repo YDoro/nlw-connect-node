cfac0e126f866b17427a58f2204ec227a2196f21
yan <yan_mvd@hotmail.com>
2025-02-24 19:05:10 -0300

    chore: :wrench: configure databases

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import z from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   CORS_ALLOWED_DOMAINS: z.string().default('*'),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
